5d51fbf080c346c1a9cbcb98356f491d3b5e1bcb
Supasit Tanapatpiboon <Rit@Supasits-MacBook-Pro.local>
2016-01-24 03:07:57 +0700

    add unity Standard Assets' Cross Platform Moblie Stick, Scripts: Player Control(For moving player using Joystick), Camera Follow Player(Making camera follows player)(not done)

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer : MonoBehaviour {
+	public Transform player;
+	public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update () 
+	{
+		transform.position = new Vector3 (player.position.x + offset.x, player.position.y + offset.y, offset.z); 
+		// Camera follows the player with specified offset position
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+// To get input from joystick.cs (MobileStick)
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveForce = 5, boostMltiplier = 2;
+	Rigidbody2D myBody;
+
+	void Start () {
+		myBody = this.GetComponents<Rigidbody2D>()[0];
+	}
+
+
+	void FixedUpdate () {
+		// Get x and y from joystick
+		Vector2 moveVec = new Vector2 (CrossPlatformInputManager.GetAxis("Horizontal"), CrossPlatformInputManager.GetAxis("Vertical")) * moveForce;
+		bool isSprinting = CrossPlatformInputManager.GetButton ("Sprint");
+		// Return True if sprint button is being pressed
+		// Return boostMltiplier if true else 1
+		Debug.Log (isSprinting ? boostMltiplier : 1);
+		// Move my adding force to the body
+		myBody.AddForce(moveVec * ( isSprinting ? boostMltiplier : 1));
+	}
+}
